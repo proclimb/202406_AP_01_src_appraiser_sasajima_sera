caed0930cf8ea00261866939685987a774d2667c
sera sasajima <202404-AP-01-sera-sasajima@x-ida.sakura.ne.jp>
2024-06-17 12:39:06 +0900

    RedminチケットNo：＃21927
    不具合状況の説明：
    「入力内容がDBに登録され、仕入管理一覧に遷移すること」とのことだが、正しい内見日付を入力してくださいのエラーメッセージが表示される。
    
    修正内容の説明：
    SRC/js/common.jsの7-20行目の間にif分が足りていなかったので11-13行目に追加修正を行いました。

--- SRC/js/common.js
+++ SRC/js/common.js
@@ -7,6 +7,10 @@
 function fnYMDCheck(msg, obj) {
 	// 未入力時はチェックしない
 	oYMD = obj.value;
+	//コードの追加11-13行目
+	if (!oYMD) {
+		return true;
+	}
 
 	var tmp = oYMD.split('/');
 	var ymd = new Date(tmp[0], parseInt(tmp[1], 10) - 1, parseInt(tmp[2], 10));
