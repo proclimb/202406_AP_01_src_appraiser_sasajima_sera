cb5feeb18ad939bc7dac6d2e2f0bf920864e226e
sera sasajima <202404-AP-01-sera-sasajima@x-ida.sakura.ne.jp>
2024-07-02 10:32:15 +0900

    RedminチケットNo：＃22807
    不具合状況の説明：
    空白文字も登録できてしまう
    
    修正内容の説明：
    SRC/js/trade.jsで36行目に修正対象箇所があり、そのままコメントアウトで残しその下の37行目に修正を行いました。

--- SRC/js/trade.js
+++ SRC/js/trade.js
@@ -33,7 +33,8 @@ function fnTradeEditCheck() {
 	}
 
 	tmp = form.zip.value;
-	if (tmp.length > 0 && !tmp.match(/^\d{3}(\s*|-)\d{4}$/)) {
+	// if (tmp.length > 0 && !tmp.match(/^\d{3}(\s*|-)\d{4}$/)) {
+	if (tmp.length > 0 && !tmp.match(/^\d{3}-*\d{4}$/)) {
 		alert("正しい郵便番号(***-**** 又は ******* )で\n入力してください");
 		return;
 	}
