afe06b946bd7b62896d845cff191f74239549dc6
sera sasajima <202404-AP-01-sera-sasajima@x-ida.sakura.ne.jp>
2024-07-04 15:53:54 +0900

    RedminチケットNo：＃22927
    不具合状況の説明：
    全て一番目に登録した内容になっている
    
    修正内容の説明：
    SRC/libDBArticle.phpの61行目に修正対象箇所があり、そのままコメントアウトで残し62行目に修正を行いました。

--- SRC/libDBArticle.php
+++ SRC/libDBArticle.php
@@ -58,7 +58,8 @@ function fnSqlArticleEdit($articleNo)
 {
 	$sql  = "SELECT ARTICLE, ROOM, KEYPLACE, ADDRESS, ARTICLENOTE, KEYBOX, DRAWING, SELLCHARGE, DEL";
 	$sql .= " FROM TBLARTICLE";
-	$sql .= " WHERE ARTICLENO = 1";
+	// $sql .= " WHERE ARTICLENO = 1";
+	$sql .= " WHERE ARTICLENO = $articleNo";
 
 	return ($sql);
 }
