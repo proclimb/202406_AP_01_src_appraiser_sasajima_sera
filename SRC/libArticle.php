35d0cffd9b69bfea2efe1ae6c4abf2523247de57
sera sasajima <202404-AP-01-sera-sasajima@x-ida.sakura.ne.jp>
2024-07-04 15:26:23 +0900

    RedminチケットNo：＃22923
    不具合状況の説明：
    ラジオボタンでなく、チェックボックスが表示されている
    
    修正内容の説明：
    SRC/libArticle.phpの228-233行目のコードが必要ないためそのままコメントアウトしました。

--- SRC/libArticle.php
+++ SRC/libArticle.php
@@ -225,17 +225,17 @@ function subArticleEdit()
 		<table border="0" cellpadding="5" cellspacing="1">
 			<tr>
 				<th>除外</th>
-				<?php if ($articleNo) { ?>
+				<!-- <//?php if ($articleNo) { ?>
 					<td>
 						<input type="checkbox" name="del" value="1" checked="checked" /> 非除外
-						<input type="checkbox" name="del" value="0" <?php if ($del == '0') print ' checked="checked"' ?> /> 除外
+						<input type="checkbox" name="del" value="0" <//?php if ($del == '0') print ' checked="checked"' ?> /> 除外
 					</td>
-				<?php } else { ?>
-					<td>
-						<input type="radio" name="del" value="1" checked="checked" /> 非除外
-						<input type="radio" name="del" value="0" <?php if ($del == '0') print ' checked="checked"' ?> /> 除外
-					</td>
-				<?php } ?>
+				<//?php } else { ?> -->
+				<td>
+					<input type="radio" name="del" value="1" checked="checked" /> 非除外
+					<input type="radio" name="del" value="0" <?php if ($del == '0') print ' checked="checked"' ?> /> 除外
+				</td>
+				<!-- <//?php } ?> -->
 			</tr>
 			<tr>
 				<th>物件名<span class="red">（必須）</span></th>
